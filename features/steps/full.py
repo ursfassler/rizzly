6aa5830858e4c0e19bfe24a7c4ec4d6e525ec803
Urs Fässler <urs@bitzgi.ch>
2015-12-03 20:59:14 +0100

    move queue test to behave

--- features/steps/full.py
+++ features/steps/full.py
@@ -66,5 +66,11 @@ def no_more_events(context):
     assert not context.testee._canRead()
 
 
+@then('I expect {count:d} events in the queue')
+def step_impl(context, count):
+    assert count == context.testee.inst__queue_count()
 
+@when('I process one event')
+def step_impl(context):
+    context.testee.inst__queue_dispatch()
 
